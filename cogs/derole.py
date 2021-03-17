d57f893a32228fecee537a93ed4a6a494518afe2
zect <s.a.s.jishu@gmail.com>
2021-03-17 20:01:57 +0900

    [Fix]: スペルミス

--- cogs/derole.py
+++ cogs/derole.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 class DeRole(commands.Cog):
-    def __init__(Self,bot):
+    def __init__(self,bot):
         self.bot = bot
         self.slash = bot.slash
 
